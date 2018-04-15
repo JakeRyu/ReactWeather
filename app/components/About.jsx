de34a8afd99ffa3c60d8bdcd17fe833a0ef7ad01
Jake Ryu <jake.ryu@hotmail.co.uk>
2018-04-15 12:42:04 +0100

    Stype examples & about pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to About page!</p>
+      <h1 className='text-center'>About</h1>
+      <p>This is a weather application built on React.</p>
+      <p>Here are some of tools I used:</p>
+      <ul>
+        <li>
+          <a href='https://facebook.github.io/react'>React</a> - This is the javascript framework used.
+        </li>
+        <li>
+          <a href='http://openweathermap.org'>Open Weather Map</a> - I used Open Weather Map to search for whether data by a city name.
+        </li>
+      </ul>
     </div>
   );
 };
